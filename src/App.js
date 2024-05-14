833ee50ea24db68afbf83db1c4c6cb9d783f03f2
iijawhor <jawhorali1@gmail.com>
2024-05-15 00:47:09 +0530

    Changes in style

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Header, Footer, Banner } from "./exports";
 import { Outlet } from "react-router-dom";
 function App() {
   return (
-    <div className="box-border m-0 p-0 bg-black text-white h-auto">
+    <div className="box-border m-0 p-0 text-white h-auto">
       <Header />
       <div className="pt-16">
         <Outlet />
