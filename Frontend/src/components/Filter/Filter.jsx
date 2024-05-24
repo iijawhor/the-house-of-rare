044d9f14f157ed487e1757b41c9d8b6aa7af7f3b
iijawhor <jawhorali1@gmail.com>
2024-05-24 17:38:01 +0530

    added css file for scrollbar and changed style in Filter.jsx file

--- Frontend/src/components/Filter/Filter.jsx
+++ Frontend/src/components/Filter/Filter.jsx
@@ -1,16 +1,22 @@
 import React from "react";
+import "./Filter.css";
 import {
   CategoryFilter,
   PriceFilter,
   ColorFilter,
   SizeFilter
 } from "../../exports";
-function Filter({ handleChange }) {
+function Filter({ handleChange, className }) {
   return (
-    <div className="flex flex-col gap-5">
+    <div
+      className={`filter flex flex-col gap-5 h-screen pb-5 overflow-y-scroll px-2 ${className}`}
+    >
       <CategoryFilter handleChange={handleChange} />
+      <hr />
       <PriceFilter handleChange={handleChange} />
+      <hr />
       <ColorFilter handleChange={handleChange} />
+      <hr />
       <SizeFilter handleChange={handleChange} />
     </div>
   );
